b4b9a9d403028d416e440f18bac1b45edcd8b948
Fauzn Ahmad <fauzanahmad0509@gmail.com>
2025-11-12 17:33:48 +0700

    Menambahkan foto-foto untuk hiasan dan menambahkan nama admin

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ export default function Logo() {
 return (
     <div className="flex items-center gap-2">
         <div className="grid h-8 w-8 place-items-center rounded-xl bg-indigo-600 text-white dark:bg-indigo-500">
-            <span className="text-xs font-bold">CO</span>
+            <span className="text-xs font-bold">RR</span>
         </div>
         <div className="leading-tight">
             <p className="text-sm font-semibold">Raffli Ramdhany</p>
